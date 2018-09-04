90aca8b6b1645b196e9cd0fa19d4af3ca775e5d1
simon <1115443660@qq.com>
2018-09-04 15:38:52 +0800

    changing to VS

--- example_pandas.py
+++ example_pandas.py
@@ -1,8 +1,17 @@
 from __future__ import print_function
 import pandas as pd
-print(pd.__version__)
+import matplotlib.pyplot as plt
+pd.set_option('display.width', None)
+
+print("pandas version-" + pd.__version__)
 example_Series_city_names = pd.Series(["San Francisco", "San Jose", "Sacramento"])
 example_Series_population = pd.Series([852469, 1015785, 485199])
 
 example_DataFrame = pd.DataFrame({"City name": example_Series_city_names, "Population": example_Series_population})
-print(example_DataFrame)
+
+example_DataFrame_from_file = pd.read_csv("california_housing_train.csv")
+# print(example_DataFrame_from_file.describe())
+# print(example_DataFrame_from_file.plot.hist('housing_median_age'))
+# plt.show()
+
+print(example_DataFrame["Population"])
---
+++ example_tensorflow_Cali_housing.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
